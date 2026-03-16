48e65d0100e60ef667e81e98aa07e4dcd8b2ef18
Shubham More <shubhammore0512@gmail.com>
2026-03-16 19:57:26 +0530

    added the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from './components/About'
 import Testimonials from './components/Testimonials'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import Customers from './components/customers'
 
 
 function App() {
